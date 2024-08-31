1a1153f475558f2968b59881bc0df68dcb5fa343
Rakibul Islam <rakibull2000@gmail.com>
2024-08-31 12:21:43 +0600

    Add ui for cardview list item

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -30,84 +30,69 @@ class DashboardPageState extends State<DashboardPage> {
   Widget build(BuildContext context) {
     final int userId = ModalRoute.of(context)!.settings.arguments as int;
     final List<String> items =
-        List<String>.generate(20, (index) => "Item $index");
+        List<String>.generate(200, (index) => "Item $index");
 
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: Column(
-        children: [
-          AppBar(
-            backgroundColor: Colors.white,
-            title: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const CircleAvatar(
-                  radius: 16,
-                  backgroundColor: Colors.black12,
-                  child: Icon(
-                    Icons.person_2_rounded,
-                    size: 24,
-                    color: Colors.grey,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              pinned: false,
+              floating: true,
+              title: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const CircleAvatar(
+                    radius: 16,
+                    backgroundColor: Colors.black12,
+                    child: Icon(
+                      Icons.person_2_rounded,
+                      size: 24,
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: FutureBuilder<String>(
-                    future: _getUserInfo(userId),
-                    builder: (context, snapshot) {
-                      return Text(
-                        snapshot.connectionState == ConnectionState.waiting
-                            ? 'Loading...'
-                            : snapshot.data ?? 'App Name',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      );
-                    },
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: FutureBuilder(
+                      future: _getUserInfo(userId),
+                      builder: (context, snapshot) {
+                        return Text(
+                          snapshot.connectionState == ConnectionState.waiting
+                              ? 'Loading...'
+                              : snapshot.data ?? 'App Name',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.logout),
-                  onPressed: () {
-                    // Handle logout event
-                  },
-                ),
-              ],
-            ),
-          ),
-          AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            toolbarHeight: 56,
-            title: TextField(
-              controller: _searchController,
-              decoration: const InputDecoration(
-                hintText: 'Search',
-                border: InputBorder.none,
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.logout))
+                ],
               ),
             ),
-            actions: [
-              IconButton(
-                onPressed: _handleSearch,
-                icon: const Icon(Icons.search),
-              )
-            ],
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Icon(Icons.label),
-                  title: Text(items[index]),
-                );
-              },
-              itemCount: items.length,
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: _SliverSearchBarDelegate(
+                searchController: _searchController,
+                onSearchPressed: _handleSearch,
+              ),
             ),
-          )
-        ],
-      )),
+            SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+              return CardExample(
+                index: index,
+              ); /*ListTile(
+                leading: const Icon(Icons.label),
+                title: Text(items[index]),
+              );*/
+            }, childCount: items.length))
+          ],
+        ),
+      ),
     );
   }
 
@@ -120,5 +105,154 @@ class DashboardPageState extends State<DashboardPage> {
     } else {
       return 'App Name'; // Return default value if user is null
     }
+  }
+}
+
+class _SliverSearchBarDelegate extends SliverPersistentHeaderDelegate {
+  final TextEditingController searchController;
+  final VoidCallback onSearchPressed;
+
+  _SliverSearchBarDelegate({
+    required this.searchController,
+    required this.onSearchPressed,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.white,
+      child: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 56,
+        title: TextField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            hintText: 'Search',
+            hintStyle:
+                TextStyle(color: Colors.grey, fontWeight: FontWeight.w300),
+            border: InputBorder.none,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: onSearchPressed,
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 56.0;
+
+  @override
+  double get minExtent => 56.0;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
+
+class CardExample extends StatelessWidget {
+  final int index;
+
+  const CardExample({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: InkWell(
+          onTap: () {
+            // Handle on tap
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                // Image section
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/images/flutter.png', // replace with your image path
+                    width: 80.0,
+                    height: 120.0,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                // Text section
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'App Name', // replace with your string
+                        style: TextStyle(
+                          fontSize: 13.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        'Placeholder Text', // replace with your string
+                        style: TextStyle(
+                          fontSize: 9.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        'Genres: S Text', // replace with your string
+                        style: TextStyle(
+                          fontSize: 9.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        'Placeholder Text', // replace with your string
+                        style: TextStyle(
+                          fontSize: 9.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        'Placeholder Text', // replace with your string
+                        style: TextStyle(
+                          fontSize: 9.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        'Schedule: S Text', // replace with your string
+                        style: TextStyle(
+                          fontSize: 9.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
