526430ab334bbcd985eede9cb95529510e42936e
Rakibul Islam <rakibull2000@gmail.com>
2024-08-31 09:55:17 +0600

    Add search bar ui and dummy list

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -5,58 +5,109 @@ import 'package:flutter_journey/database/model/db_entities.dart';
 
 import '../database/database_helper.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  DashboardPageState createState() => DashboardPageState();
+}
+
+class DashboardPageState extends State<DashboardPage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  void _handleSearch() {
+    String searchText = _searchController.text;
+    log('SearchText: $searchText');
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose(); // Dispose the controller to free up resources
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final int userId = ModalRoute.of(context)!.settings.arguments as int;
+    final List<String> items =
+        List<String>.generate(20, (index) => "Item $index");
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              radius: 16,
-              backgroundColor: Colors.black12,
-              child: Icon(
-                Icons.person_2_rounded,
-                size: 24,
-                color: Colors.grey,
+      body: SafeArea(
+          child: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.white,
+            title: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 16,
+                  backgroundColor: Colors.black12,
+                  child: Icon(
+                    Icons.person_2_rounded,
+                    size: 24,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: FutureBuilder<String>(
+                    future: _getUserInfo(userId),
+                    builder: (context, snapshot) {
+                      return Text(
+                        snapshot.connectionState == ConnectionState.waiting
+                            ? 'Loading...'
+                            : snapshot.data ?? 'App Name',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.logout),
+                  onPressed: () {
+                    // Handle logout event
+                  },
+                ),
+              ],
+            ),
+          ),
+          AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            toolbarHeight: 56,
+            title: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                border: InputBorder.none,
               ),
             ),
-            const SizedBox(width: 8),
-            Expanded(
-              child: FutureBuilder<String>(
-                future: _getUserInfo(userId),
-                builder: (context, snapshot) {
-                  return Text(
-                    snapshot.connectionState == ConnectionState.waiting
-                        ? 'Loading...'
-                        : snapshot.data ?? 'App Name',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                },
-              ),
-            ),
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () {
-                // Handle logout event
+            actions: [
+              IconButton(
+                onPressed: _handleSearch,
+                icon: const Icon(Icons.search),
+              )
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.label),
+                  title: Text(items[index]),
+                );
               },
+              itemCount: items.length,
             ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Text('Welcome, User $userId'),
-      ),
+          )
+        ],
+      )),
     );
   }
 
